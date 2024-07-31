a3b613269483b6335bedbf91f6d5a7d0979cd5e1
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-31 10:23:06 +0700

    Only specific tags are useful. Need to make it tighter

--- profiles/custom.lua
+++ profiles/custom.lua
@@ -269,80 +269,15 @@ function custom_tag_weights(profile, way, result, data)
   local crossing = way:get_value_by_key('crossing')
   local footway = way:get_value_by_key('footway')
   local highway = way:get_value_by_key('highway')
-  local oneway = way:get_value_by_key('oneway')
-  local surface = way:get_value_by_key('surface')
-  if crossing == 'uncontrolled' then
+  if crossing == 'traffic_signals' then
      result.forward_speed = walking_speed * 100.0
      result.backward_speed = walking_speed * 100.0
-  elseif crossing == 'traffic_signals' then
-     result.forward_speed = walking_speed * 92.8212478920742
-     result.backward_speed = walking_speed * 92.8212478920742
-  elseif crossing == 'marked' then
-     result.forward_speed = walking_speed * 91.65261382799324
-     result.backward_speed = walking_speed * 91.65261382799324
-  elseif crossing == 'unmarked' then
-     result.forward_speed = walking_speed * 22.70320404721754
-     result.backward_speed = walking_speed * 22.70320404721754
   elseif footway == 'sidewalk' then
      result.forward_speed = walking_speed * 100.0
      result.backward_speed = walking_speed * 100.0
-  elseif highway == 'service' then
-     result.forward_speed = walking_speed * 100.0
-     result.backward_speed = walking_speed * 100.0
-  elseif highway == 'residential' then
-     result.forward_speed = walking_speed * 63.39966528530443
-     result.backward_speed = walking_speed * 63.39966528530443
   elseif highway == 'footway' then
-     result.forward_speed = walking_speed * 55.47840293273829
-     result.backward_speed = walking_speed * 55.47840293273829
-  elseif highway == 'tertiary' then
-     result.forward_speed = walking_speed * 7.44588779088301
-     result.backward_speed = walking_speed * 7.44588779088301
-  elseif highway == 'primary' then
-     result.forward_speed = walking_speed * 7.106630538731272
-     result.backward_speed = walking_speed * 7.106630538731272
-  elseif highway == 'secondary' then
-     result.forward_speed = walking_speed * 3.7219477207523113
-     result.backward_speed = walking_speed * 3.7219477207523113
-  elseif highway == 'primary_link' then
-     result.forward_speed = walking_speed * 2.4832642652215493
-     result.backward_speed = walking_speed * 2.4832642652215493
-  elseif highway == 'steps' then
-     result.forward_speed = walking_speed * 2.3254701944532994
-     result.backward_speed = walking_speed * 2.3254701944532994
-  elseif highway == 'pedestrian' then
-     result.forward_speed = walking_speed * 1.5286101370736374
-     result.backward_speed = walking_speed * 1.5286101370736374
-  elseif highway == 'path' then
-     result.forward_speed = walking_speed * 1.4497131016895124
-     result.backward_speed = walking_speed * 1.4497131016895124
-  elseif highway == 'secondary_link' then
-     result.forward_speed = walking_speed * 1.3708160663053874
-     result.backward_speed = walking_speed * 1.3708160663053874
-  elseif highway == 'tertiary_link' then
-     result.forward_speed = walking_speed * 1.142014663691425
-     result.backward_speed = walking_speed * 1.142014663691425
-  elseif highway == 'trunk_link' then
-     result.forward_speed = walking_speed * 1.142014663691425
-     result.backward_speed = walking_speed * 1.142014663691425
-  elseif oneway == 'yes' then
      result.forward_speed = walking_speed * 100.0
      result.backward_speed = walking_speed * 100.0
-  elseif surface == 'asphalt' then
-     result.forward_speed = walking_speed * 100.0
-     result.backward_speed = walking_speed * 100.0
-  elseif surface == 'concrete' then
-     result.forward_speed = walking_speed * 9.15934065934066
-     result.backward_speed = walking_speed * 9.15934065934066
-  elseif surface == 'paving_stones' then
-     result.forward_speed = walking_speed * 5.181662087912088
-     result.backward_speed = walking_speed * 5.181662087912088
-  elseif surface == 'concrete:plates' then
-     result.forward_speed = walking_speed * 4.365728021978022
-     result.backward_speed = walking_speed * 4.365728021978022
-  elseif surface == 'unpaved' then
-     result.forward_speed = walking_speed * 1.1699862637362637
-     result.backward_speed = walking_speed * 1.1699862637362637
   else
     result.forward_speed = walking_speed
     result.backward_speed = walking_speed
